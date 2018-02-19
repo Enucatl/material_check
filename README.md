3b72acce461f463acacd6f6e6ca1be77b1f9c224
Matteo Abis <gmatteo..abis@gmail.com>
2018-02-19 14:35:45 +0100

    print comparison

---
+++ README.md
@@ -0,0 +1,2 @@
+python material_check.py /sls/X02DA/data/e13657/Data10/matteo_high_resolution/KO202_LL_control_1/tif/ KO202.npy --height_map_left /sls/X02DA/data/e13657/Data10/matteo_high_resolution/stitched_datasets/heightmaps/KO202_LL_control_left.tif --height_map_right /sls/X02DA/data/e13657/Data10/matteo_high_resolution/stitched_datasets/heightmaps/KO202_LL_control_right.tif
+
--- material_check.py
+++ material_check.py
@@ -4,15 +4,26 @@ import numpy as np
 from glob import glob
 from tqdm import tqdm
 import tifffile
+import nist_lookup.xraydb_plugin as xdb
 
 
 @click.command()
 @click.argument(
     "folder",
     type=click.Path(exists=True, file_okay=False))
-def main(folder):
+@click.option(
+    "--height_map_left",
+    type=click.Path(exists=True))
+@click.option(
+    "--height_map_right",
+    type=click.Path(exists=True))
+@click.argument(
+    "output",
+    type=click.Path())
+def main(folder, height_map_left, height_map_right, output):
     input_files = sorted(glob(
         os.path.join(folder, "*.tif")))
+    thickness = tifffile.imread(height_map_right) - tifffile.imread(height_map_left)
     darks = input_files[1:30]
     flats = input_files[30:130]
     first_projection = input_files[130]
@@ -29,6 +40,18 @@ def main(folder):
     first_projection = tifffile.imread(first_projection)
     print(flat.shape)
     print(first_projection.shape)
+    a = (first_projection - dark) / (flat - dark)
+    t = 3400
+    d = 0.18
+    pixel_size = 0.65e-6
+    mu = -np.log(a[:, :thickness.shape[1]]) / (t * d * pixel_size)
+    np.save(output, mu)
+    _, _, atlen = xdb.xray_delta_beta("CH12", 2, 10e3)
+    mu_theory = 1 / (atlen * 1e-2)
+    print(np.median(mu), np.std(mu))
+    print(mu_theory)
+
+
 
 
 if __name__ == "__main__":
